1f990268b1d325b04c730bfa5b9fdc03186fd75d
Tommaso <tommaso.macco@gmail.com>
2021-11-01 14:22:16 +0100

    jsnack2 fix

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,18 +2,18 @@
 
 
 // Chiediamo all'utente le parole
-let parola1 = prompt ("Inserisci la prima parola")
-let parola2 = prompt ("Inserisci la seconda parola")
+const parola1 = prompt("Inserisci la prima parola.");
+const parola2 = prompt("Inserisci la seconda parola.");
 
-// COnfrontiamo le parole
+console.log("La prima parola è: " + parola1);
+console.log("La seconda parola è: " + parola2);
 
-if (parola1.length > parola2.length) {
-    console.log("la parola più corta è:", parola2 "e la più lunga è:", parola1);
-}
-else if (parola1.length < parola2.length) {
-    console.log("la parola più corta è:", parola1 "e la più lunga è:", parola2);
-}
-else {
-    console.log("scrivi due parole non lunghe uguali!");
+// confrontiamo le parole
+if (parola1.length == parola2.length) {
+    console.log("Le due parole hanno la stessa lunghezza");
+} else if (parola1.length > parola2.length) {
+    console.log("la parola più corta è", parola2, "la parola più lunga è", parola1);
+} else  {
+    console.log("la parola più corta è", parola1, "la parola più lunga è", parola2);
 }
 
