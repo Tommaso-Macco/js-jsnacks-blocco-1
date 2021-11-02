09460a5e51a3d1d21b9470933586268a9576be76
Tommaso <tommaso.macco@gmail.com>
2021-11-02 10:04:56 +0100

    jsnack3 da fixare

---
+++ jsnack3/script.js
@@ -0,0 +1,14 @@
+var arrNums = [];
+let somma = 0;
+
+for (let i = 0; i < 10; i++) {
+    arrNums.push(prompt("inserisci un numero x10"));
+}
+console.log(arrNums);
+
+for (let i = 0; i < arrNums.length; i++) {
+
+}
+
+console.log(somma);
+// console.log(arrNums);
